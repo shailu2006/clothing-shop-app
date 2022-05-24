39b0b7c9978af7ad65065ea514e6db231dcfa3e4
Shailesh <shailu2006@gmail.com>
2022-05-24 17:14:40 +0530

    directory changes

---
+++ src/components/directory/directory-component.jsx
@@ -0,0 +1,14 @@
+import Category from "../category/Category.component";
+import "./directory-styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
